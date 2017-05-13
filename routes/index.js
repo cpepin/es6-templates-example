78d14eba60c8b1dd3b4a3021dbddc2814105b783
cpepin <pepin.cameron@gmail.com>
2017-05-13 11:33:09 -0400

    Started working with pug files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const cam = { name: 'Cam', age: 23, cool: true };
+  // res.send('Hey! It works!');
+  // res.send(req.query);
+  res.render('hello', {
+    name: 'Cam',
+    cat: 'Chaplin'
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name];
+  reverse.reverse();
+  res.send(reverse.join(''));
 });
 
 module.exports = router;
