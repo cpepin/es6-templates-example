63025f722bb7202b1851b9ac89d9dc4bde86f4c2
cpepin <pepin.cameron@gmail.com>
2017-05-13 11:48:23 -0400

    Adding first controller

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const cam = { name: 'Cam', age: 23, cool: true };
-  // res.send('Hey! It works!');
-  // res.send(req.query);
-  res.render('hello', {
-    name: 'Cam',
-    cat: 'Chaplin'
-  });
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name];
